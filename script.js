d57e1fe13f384ca34853967941031a157fc23849
Dhanush CK <dhanushck01@gmail.com>
2024-02-10 17:32:37 +0530

    Made button working

--- script.js
+++ script.js
@@ -1,21 +1,35 @@
-let questions = ["Who is the father of Computer?","Who is the father of Artificial Intelligance?"];
-let answers = [{q1:["Charles Babbage", "Alan Turing", "Konrad Zuse", "Ada Lovelavce"]},"Alan Turing"];
+let questions = ["Who is the father of Computer?","How many Bits make one Byte?", "Which electronic component was used in the first generation of computers?"];
+const optns = [["Charles Babbage", "Alan Turing", "Konrad Zuse", "Ada Lovelavce"], ["16 Bits", "32 Bits", "8 Bits", "64 Bits"], ["Vaccum Tube", "Transistor", "Microprocessor", "Integrated Circuit(IC)"]];
+let answers = ["Charles Babbage", "Alan Turing", "Vaccum Tube"];
 
 var que = document.getElementById('que');
 var a = document.getElementById('a');
 var b = document.getElementById('b');
 var c = document.getElementById('c');
 var d = document.getElementById('d');
+const nxt = document.getElementById('next');
 let options = [a,b,c,d];
-// document.que.innerHTML = questions[0];
+
+let i=1;
+let k;
+
+nxt.addEventListener('click', (e)=> {
+    if(i<questions.length){
+        k=i;
+    }
+    while(i<=k) {
+        for(let j=0;j<4;j++) {
+            que.innerHTML = questions[i];
+            options[j].innerHTML = optns[i][j];
+        } 
+        i++;
+    }
+});
 
 function update() {
-    que.innerHTML = questions[0];
-
+    for(let j=0;j<4;j++) {
+        i=1;
+        que.innerHTML = questions[0];
+        options[j].innerHTML = optns[0][j];
+    }
 }
-// for (let i=0;i<questions.length;i++) {
-
-// }
-// do {
-
-// }
