360afa59dc07e42bab2048232c7dc38a22bb8946
Dhanush CK <dhanushck01@gmail.com>
2024-02-10 18:37:09 +0530

    Updated next button working

--- script.js
+++ script.js
@@ -1,35 +1,58 @@
 let questions = ["Who is the father of Computer?","How many Bits make one Byte?", "Which electronic component was used in the first generation of computers?"];
 const optns = [["Charles Babbage", "Alan Turing", "Konrad Zuse", "Ada Lovelavce"], ["16 Bits", "32 Bits", "8 Bits", "64 Bits"], ["Vaccum Tube", "Transistor", "Microprocessor", "Integrated Circuit(IC)"]];
-let answers = ["Charles Babbage", "Alan Turing", "Vaccum Tube"];
+let answers = ["Charles Babbage", "8 Bits", "Vaccum Tube"];
 
 var que = document.getElementById('que');
+var result = document.getElementById('result');
+
 var a = document.getElementById('a');
 var b = document.getElementById('b');
 var c = document.getElementById('c');
 var d = document.getElementById('d');
 const nxt = document.getElementById('next');
-let options = [a,b,c,d];
+let options = [a, b, c, d];
 
 let i=1;
-let k;
+let k=0;
 
 nxt.addEventListener('click', (e)=> {
-    if(i<questions.length){
-        k=i;
-    }
-    while(i<=k) {
-        for(let j=0;j<4;j++) {
-            que.innerHTML = questions[i];
-            options[j].innerHTML = optns[i][j];
-        } 
-        i++;
+    if (result.innerHTML === "Right Answer") {
+        result.innerHTML = "";
+        if(i<questions.length){
+            k=i;
+        }
+        while(i<=k) {
+            for(let j=0;j<4;j++) {
+                que.innerHTML = questions[i];
+                options[j].innerHTML = optns[i][j];
+            } 
+            i++;
+        }
     }
 });
 
 function update() {
     for(let j=0;j<4;j++) {
         i=1;
+        k=0;
         que.innerHTML = questions[0];
         options[j].innerHTML = optns[0][j];
     }
 }
+
+function hide() {
+    document.getElementById('start').style.display = 'none';
+}
+
+const option = ['a', 'b', 'c', 'd'];
+option.forEach(buttonId=> {
+    const opt = document.getElementById(buttonId);
+    opt.addEventListener('click', (e)=> {
+        console.log(k);
+        console.log(answers[k]);
+        if (e.target.innerHTML === answers[k])
+            result.innerHTML = "Right Answer";
+        else
+            result.innerHTML = "Wrong Answer";
+    })
+});
