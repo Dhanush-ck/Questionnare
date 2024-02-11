e8e54d8faee48108fbdc0fb0b675c6bcea2228e0
Dhanush CK <dhanushck01@gmail.com>
2024-02-11 13:56:16 +0530

    Updated button's working flow

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
-let questions = ["Who is the father of Computer?","How many Bits make one Byte?", "Which electronic component was used in the first generation of computers?"];
-const optns = [["Charles Babbage", "Alan Turing", "Konrad Zuse", "Ada Lovelavce"], ["16 Bits", "32 Bits", "8 Bits", "64 Bits"], ["Vaccum Tube", "Transistor", "Microprocessor", "Integrated Circuit(IC)"]];
-let answers = ["Charles Babbage", "8 Bits", "Vaccum Tube"];
+let questions = ["Who is the father of Computer?","How many Bits make one Byte?", "Which electronic component was used in the first generation of computers?", "What is the full form of RAM?"];
+const optns = [["Charles Babbage", "Alan Turing", "Konrad Zuse", "Ada Lovelavce"], ["16 Bits", "32 Bits", "8 Bits", "64 Bits"], ["Vaccum Tube", "Transistor", "Microprocessor", "Integrated Circuit(IC)"], ["Random Applicable Memory", "Read Access Memory", "Read Applicable Memory", "Random Acces Memory"]];
+let answers = ["Charles Babbage", "8 Bits", "Vaccum Tube", "Random Acces Memory"];
 
 var que = document.getElementById('que');
 var result = document.getElementById('result');
 const modal = document.getElementById('modal');
+let message = document.getElementById('message');
 
 var a = document.getElementById('a');
 var b = document.getElementById('b');
@@ -13,12 +14,25 @@ var d = document.getElementById('d');
 const nxt = document.getElementById('next');
 let options = [a, b, c, d];
 
-let i=1;
+let i=0;
 let k=0;
 
-nxt.addEventListener('click', (e)=> {
+
+function next() {
     modal.close();
-    if (result.innerHTML === "Right Answer") {
+    if (result.innerHTML === "") {
+        if(i<questions.length){
+            k=i;
+        }
+        while(i<=k) {
+            for(let j=0;j<4;j++) {
+                que.innerHTML = questions[i];
+                options[j].innerHTML = optns[i][j];
+            } 
+            i++;
+        }
+    }
+    if ((result.innerHTML === "Right Answer")&&(k !== questions.length-1)) {
         result.innerHTML = "";
         if(i<questions.length){
             k=i;
@@ -31,19 +45,35 @@ nxt.addEventListener('click', (e)=> {
             i++;
         }
     }
-});
-
-function update() {
-    for(let j=0;j<4;j++) {
-        i=1;
-        k=0;
-        que.innerHTML = questions[0];
-        options[j].innerHTML = optns[0][j];
+    if ((result.innerHTML === "Right Answer")&&(k===questions.length-1)) {
+        message.innerHTML = "You Won";
+        que.style.display = 'none';
+        let arr = Array.from(option);
+        arr.forEach(buttonId=> {
+            const opt = document.getElementById(buttonId);
+            opt.style.display = 'none';
+        });
+        document.getElementById('hide').style.display = 'flex';
+        document.getElementById('start').innerHTML = 'Play Again';
+        i=0, k=0;
     }
+    // console.log(k);
+    // console.log(result.innerHTML);
 }
+
 
 function hide() {
     document.getElementById('hide').style.display = 'none';
+    
+}
+
+function show() {
+    que.style.display = 'flex';
+    let arr = Array.from(option);
+    arr.forEach(buttonId=> {
+        const opt = document.getElementById(buttonId);
+        opt.style.display = 'block';
+    });
 }
 
 const option = ['a', 'b', 'c', 'd'];
@@ -51,8 +81,8 @@ option.forEach(buttonId=> {
     const opt = document.getElementById(buttonId);
     opt.addEventListener('click', (e)=> {
         modal.showModal();
-        console.log(k);
-        console.log(answers[k]);
+        // console.log(k);
+        // console.log(answers[k]);
         if (e.target.innerHTML === answers[k])
             result.innerHTML = "Right Answer";
         else
