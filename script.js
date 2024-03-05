5e32e92a6403371b11d2ded006d81636ae15b787
Dhanush CK <dhanushck01@gmail.com>
2024-03-05 22:40:24 +0530

    Added New Question

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-let questions = ["Who is the father of Computer?","How many Bits make one Byte?", "Which electronic component was used in the first generation of computers?", "What is the full form of RAM?", "An address given to a computer connected to a network is called ?", "C, Java, C++ and PHP are examples for?"];
-const optns = [["Charles Babbage", "Alan Turing", "Konrad Zuse", "Ada Lovelavce"], ["16 Bits", "32 Bits", "8 Bits", "64 Bits"], ["Vaccum Tube", "Transistor", "Microprocessor", "Integrated Circuit(IC)"], ["Random Applicable Memory", "Read Access Memory", "Read Applicable Memory", "Random Acces Memory"], ["Local address", "Localhost", "Network address", "IP adderss"], ["Software Types", "Firmware", "Programming Languages", "Programming Translators"]];
-let answers = ["Charles Babbage", "8 Bits", "Vaccum Tube", "Random Acces Memory", "IP adderss", "Programming Languages"];
+let questions = ["Who is the father of Computer?","How many Bits make one Byte?", "Which electronic component was used in the first generation of computers?", "What is the full form of RAM?", "An address given to a computer connected to a network is called ?", "C, Java, C++ and PHP are examples for?", "The term 'Pentium' is related to?"];
+const optns = [["Charles Babbage", "Alan Turing", "Konrad Zuse", "Ada Lovelavce"], ["16 Bits", "32 Bits", "8 Bits", "64 Bits"], ["Vaccum Tube", "Transistor", "Microprocessor", "Integrated Circuit(IC)"], ["Random Applicable Memory", "Read Access Memory", "Read Applicable Memory", "Random Acces Memory"], ["Local address", "Localhost", "Network address", "IP adderss"], ["Software Types", "Firmware", "Programming Languages", "Programming Translators"], ["Microprocessor", "CMOS", "Motherboard", "OS"]];
+let answers = ["Charles Babbage", "8 Bits", "Vaccum Tube", "Random Acces Memory", "IP adderss", "Programming Languages", "Microprocessor"];
 
 var que = document.getElementById('que');
 var result = document.getElementById('result');
