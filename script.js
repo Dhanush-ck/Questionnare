5371ed488ff0f88dd3bfba2cf2812b6c063f2560
Dhanush CK <dhanushck01@gmail.com>
2024-03-11 22:42:52 +0530

    Updated Question Setup

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
-let questions = ["Who is the father of Computer?","How many Bits make one Byte?", "Which electronic component was used in the first generation of computers?", "What is the full form of RAM?", "An address given to a computer connected to a network is called ?", "C, Java, C++ and PHP are examples for?", "The term 'Pentium' is related to?"];
-const optns = [["Charles Babbage", "Alan Turing", "Konrad Zuse", "Ada Lovelavce"], ["16 Bits", "32 Bits", "8 Bits", "64 Bits"], ["Vaccum Tube", "Transistor", "Microprocessor", "Integrated Circuit(IC)"], ["Random Applicable Memory", "Read Access Memory", "Read Applicable Memory", "Random Acces Memory"], ["Local address", "Localhost", "Network address", "IP adderss"], ["Software Types", "Firmware", "Programming Languages", "Programming Translators"], ["Microprocessor", "CMOS", "Motherboard", "OS"]];
-let answers = ["Charles Babbage", "8 Bits", "Vaccum Tube", "Random Acces Memory", "IP adderss", "Programming Languages", "Microprocessor"];
+// let questions = ["Who is the father of Computer?","How many Bits make one Byte?", "Which electronic component was used in the first generation of computers?", "What is the full form of RAM?", "An address given to a computer connected to a network is called ?", "C, Java, C++ and PHP are examples for?", "The term 'Pentium' is related to?"];
+// const optns = [["Charles Babbage", "Alan Turing", "Konrad Zuse", "Ada Lovelavce"], ["16 Bits", "32 Bits", "8 Bits", "64 Bits"], ["Vaccum Tube", "Transistor", "Microprocessor", "Integrated Circuit(IC)"], ["Random Applicable Memory", "Read Access Memory", "Read Applicable Memory", "Random Acces Memory"], ["Local address", "Localhost", "Network address", "IP adderss"], ["Software Types", "Firmware", "Programming Languages", "Programming Translators"], ["Microprocessor", "CMOS", "Motherboard", "OS"]];
+// let answers = ["Charles Babbage", "8 Bits", "Vaccum Tube", "Random Acces Memory", "IP adderss", "Programming Languages", "Microprocessor"];
+
+//questions
+
+const question = { questions : ["Who is the father of Computer?","How many Bits make one Byte?", "Which electronic component was used in the first generation of computers?", "What is the full form of RAM?", "An address given to a computer connected to a network is called ?", "C, Java, C++ and PHP are examples for?", "The term 'Pentium' is related to?"], 
+answers : ["Charles Babbage", "8 Bits", "Vaccum Tube", "Random Acces Memory", "IP adderss", "Programming Languages", "Microprocessor"], 
+optns : [{optn : ["Charles Babbage", "Alan Turing", "Konrad Zuse", "Ada Lovelavce"]}, {optn : ["16 Bits", "32 Bits", "8 Bits", "64 Bits"]}, {optn : ["Vaccum Tube", "Transistor", "Microprocessor", "Integrated Circuit(IC)"]}, {optn: ["Random Applicable Memory", "Read Access Memory", "Read Applicable Memory", "Random Acces Memory"]}, {optn : ["Local address", "Localhost", "Network address", "IP adderss"]}, {optn : ["Software Types", "Firmware", "Programming Languages", "Programming Translators"]}, {optn : ["Microprocessor", "CMOS", "Motherboard", "OS"]}] };
+
+//variable declarations
 
 var que = document.getElementById('que');
 var result = document.getElementById('result');
@@ -17,35 +25,42 @@ let options = [a, b, c, d];
 let i=0;
 let k=0;
 
+//next() 
 
 function next() {
-    modal.close();
+    modal.close();//modal closing
+
+    // question displaying
     if (result.innerHTML === "") {
-        if(i<questions.length){
+        if(i<question.questions.length){
             k=i;
         }
         while(i<=k) {
             for(let j=0;j<4;j++) {
-                que.innerHTML = questions[i];
-                options[j].innerHTML = optns[i][j];
+                que.innerHTML = question.questions[i];
+                options[j].innerHTML = question.optns[i].optn[j];
             } 
             i++;
         }
     }
-    if ((result.innerHTML === "Right Answer")&&(k !== questions.length-1)) {
+
+    // Checking whether it is not last question
+    if ((result.innerHTML === "Right Answer")&&(k !== question.questions.length-1)) {
         result.innerHTML = "";
-        if(i<questions.length){
+        if(i<question.questions.length){
             k=i;
         }
         while(i<=k) {
             for(let j=0;j<4;j++) {
-                que.innerHTML = questions[i];
-                options[j].innerHTML = optns[i][j];
+                que.innerHTML = question.questions[i];
+                options[j].innerHTML = question.optns[i].optn[j];
             } 
             i++;
         }
     }
-    if ((result.innerHTML === "Right Answer")&&(k===questions.length-1)) {
+
+    // Checking whether it is the last question 
+    if ((result.innerHTML === "Right Answer")&&(k=== question.questions.length-1)) {
         message.innerHTML = "You Won";
         que.style.display = 'none';
         let arr = Array.from(option);
@@ -57,6 +72,8 @@ function next() {
         document.getElementById('start').innerHTML = 'Play Again';
         i=0, k=0;
     }
+
+    // Wrong answer condition 
     if (result.innerHTML === "Wrong Answer") {
         message.innerHTML = "You Got";
         que.style.display = 'none';
@@ -73,11 +90,14 @@ function next() {
     // console.log(result.innerHTML);
 }
 
+//hide()
 
 function hide() {
     document.getElementById('hide').style.display = 'none';
     
 }
+
+//show()
 
 function show() {
     que.style.display = 'flex';
@@ -88,17 +108,19 @@ function show() {
     });
 }
 
+//answer checking
+
 const option = ['a', 'b', 'c', 'd'];
 option.forEach(buttonId=> {
     const opt = document.getElementById(buttonId);
     opt.addEventListener('click', (e)=> {
-        if ((e.target.innerHTML === answers[k])&&(k===questions.length-1)) {
+        if ((e.target.innerHTML === question.answers[k])&&(k===question.questions.length-1)) {
             nxt.innerHTML = "Show Result";
         }
         modal.showModal();
         // console.log(k);
         // console.log(answers[k]);
-        if (e.target.innerHTML === answers[k]) {
+        if (e.target.innerHTML === question.answers[k]) {
             result.innerHTML = "Right Answer";
             e.target.style.backgroundColor = "Green";
         }
